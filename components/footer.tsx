43efe92e54a73c71587c98e78f2fd97a698ad3aa
lerry <lvdachao@gmail.com>
2024-08-24 19:47:01 +0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,9 +9,6 @@ export default function Footer() {
   return (
     <footer className="flex w-full flex-col">
       <div className="mx-auto w-full max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex flex-col items-center justify-center gap-2 md:order-2 md:items-end">
-          <ThemeSwitch />
-        </div>
         <div className="mt-4 md:order-1 md:mt-0">
           <div className="flex items-center justify-center gap-3 md:justify-start">
             <div className="flex items-center">
